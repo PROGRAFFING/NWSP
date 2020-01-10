aa618e99d850d50222dc40530c56dd2c08a5d1e3
Filip <filip_frontend@gmail.com>
2020-01-10 16:57:46 +0100

    Add blog section home page

--- page.php
+++ page.php
@@ -26,8 +26,16 @@ $context = Timber::context();
 $featured = Timber::get_posts(array('post_type' => 'post', 'category_name' => 'wyroznione', 'posts_per_page' =>  2)); // uses 
 
 
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 $context['featured'] = $featured;
+
+$argsQuery = array(
+    'posts_per_page'   => 6,
+    'post_type'        => 'posts',
+    'category__not_in' =>  4,
+);
+$context['posts'] = Timber::get_posts(array('post_type' => 'post', 'posts_per_page' =>  6, 'category__not_in' => 4));
 
 Timber::render( array( 'pages/page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
