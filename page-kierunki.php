83e74629c49ec2cf8b9aa0905c33dcb0d4a8bebe
Filip <filip_frontend@gmail.com>
2020-01-20 15:24:27 +0100

    Add "oferta edukacyjna"

--- page-kierunki.php
+++ page-kierunki.php
@@ -8,10 +8,8 @@
 
 $context = Timber::context();
 
-
-
-
 $timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
 
 
-Timber::render( array( 'pages/page-kierunki.twig', 'page.twig' ), $context );
+Timber::render( array( 'pages/page-kierunki.twig' ), $context );
